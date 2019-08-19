b435d77e3ca2499e19a13c4adf00965d20f32c85
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-19 06:37:38 +0545

    feat: Add the video snippet details

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import {Paper} from '@material-ui/core';
+import {Paper, Typography} from '@material-ui/core';
 
 const VideoDetail = ({video}) => {
   if (!video) return <div>Loading...</div>;
@@ -16,6 +16,15 @@ const VideoDetail = ({video}) => {
           title="Video Player"
           src={videoSrc}
         />
+      </Paper>
+      <Paper elevation={6} style={{padding: '15px'}}>
+        <Typography variant="h4">
+          {video.snippet.title} - {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle1">
+          {video.snippet.channelTitle}
+        </Typography>
+        <Typography variant="subtitle2">{video.snippet.description}</Typography>
       </Paper>
     </React.Fragment>
   );
