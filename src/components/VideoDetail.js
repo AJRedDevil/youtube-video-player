07fcc295f3ca1cc6332dee62d4c127a19ea51180
Ajan Lal Shrestha <ajan.shresh@gmail.com>
2019-08-19 06:37:17 +0545

    feat: Fetch Video
    - Embed video into iframe
    - Send the video as props to VideoDetail

--- src/components/VideoDetail.js
+++ src/components/VideoDetail.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
-const VideoDetail = () => <h1>This is a VideoDetail Compontent</h1>;
+import {Paper} from '@material-ui/core';
+
+const VideoDetail = ({video}) => {
+  if (!video) return <div>Loading...</div>;
+
+  const videoSrc = `https://www.youtube.com/embed/${video.id.videoId}`;
+  return (
+    <React.Fragment>
+      <Paper elevation={6} style={{height: '70%'}}>
+        <iframe
+          frameBorder="0"
+          height="100%"
+          width="100%"
+          title="Video Player"
+          src={videoSrc}
+        />
+      </Paper>
+    </React.Fragment>
+  );
+};
 
 export default VideoDetail;
